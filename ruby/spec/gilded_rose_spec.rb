05960d8820b35364ddc1f4957f8a94b487bbc4a0
frankie <franklin.shaw@gmail.com>
2017-03-07 16:17:31 +0000

    added special item Conjured

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -11,7 +11,7 @@ describe GildedRose do
 
     context "Regular Items & special item's normal charactersitics" do
 
-      it 'quality decreases over time' do
+      it 'quality decreases by 1' do
         items = [Item.new("foo", 10, 5)]
         GildedRose.new(items).update_quality
         expect(items[0].quality).to eq 4
@@ -29,7 +29,7 @@ describe GildedRose do
         expect(items[0].quality).to eq 0
       end
 
-      it 'sell_in decreases over time' do
+      it 'sell_in decreases by 1' do
         items = [Item.new("foo", 10, 5)]
         GildedRose.new(items).update_quality
         expect(items[0].sell_in).to eq 9
@@ -45,16 +45,15 @@ describe GildedRose do
         expect(items[0].quality).to eq 50
       end
 
-
       context "Sulfuras" do
 
-        it 'sell_in does not decrease over time' do
+        it 'sell_in does not decrease' do
           items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 5)]
           GildedRose.new(items).update_quality
           expect(items[0].sell_in).to eq 10
         end
 
-        it 'quality does not decrease over time' do
+        it 'quality does not decrease' do
           items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 5)]
           GildedRose.new(items).update_quality
           expect(items[0].quality).to eq 5
@@ -64,7 +63,7 @@ describe GildedRose do
 
       context "Backstage Passes" do
 
-        it 'quality increases by 1 over time' do
+        it 'quality increases by 1' do
           items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 5)]
           GildedRose.new(items).update_quality
           expect(items[0].quality).to eq 6
@@ -87,19 +86,47 @@ describe GildedRose do
           GildedRose.new(items).update_quality
           expect(items[0].quality).to eq 0
         end
-
       end
 
       context "Aged Brie" do
 
-        it 'quality increases over time' do
+        it 'quality increases by 1' do
           items = [Item.new("Aged Brie", 10, 5)]
           GildedRose.new(items).update_quality
           expect(items[0].quality).to eq 6
         end
 
       end
+    end
 
+    context "Added items" do
+      context "Conjured" do
+
+        it 'quality decreases by 2' do
+          items = [Item.new("Conjured fucks to give", 10, 8)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq 6
+        end
+
+        it 'quality decreases 2x after sell_in' do
+          items = [Item.new("Conjured fucks to give", 0, 5)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq 1
+        end
+
+        it 'quality is never < 0' do
+          items = [Item.new("Conjured fucks to give", 10, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq 0
+        end
+
+        it 'sell_in decreases by 1' do
+          items = [Item.new("Conjured fucks to give", 10, 5)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq 9
+        end
+
+      end
     end
 
   end
